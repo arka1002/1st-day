7b37a08211f367805cd1883f62b99f31fbf3adc5
arka1002 <arka.mcu700@gmail.com>
2023-02-17 09:20:45 +0530

    finally done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { db } from "./db";
 import { useLiveQuery } from 'dexie-react-hooks';
 import { AiOutlineHome, AiOutlineUser, AiOutlineSetting, AiOutlineMessage, AiOutlineBell } from "react-icons/ai";
+import { MdOutlineWavingHand } from "react-icons/md";
 
 function App() {
   const [revenue, setRevenue] = useState("");
@@ -33,34 +34,46 @@ function App() {
         <div className="profile cursor-pointer">
           <div className="profile-icon w-[80px] h-[80px] bg-red-500 mt-[20px] mx-auto border-4 border-white rounded-full"></div>
         </div>
-        <div className="list-icon text-white flex flex-row justify-center mt-[10px] p-4 cursor-pointer">
-          <AiOutlineHome className="font-semibold text-xl block mt-[3px]" />
-          <p className="block text-lg font-semibold pl-[10px]">Create Company</p>
+
+        <div className="pl-[79px] pt-[6px]">
+
+          <div className="list-icon text-white flex flex-row justify-start mt-[10px] p-4 cursor-pointer">
+            <AiOutlineHome className="font-semibold text-xl block mt-[3px]" />
+            <p className="block text-lg font-semibold pl-[10px]">Create Company</p>
+          </div>
+          <div className="list-icon text-white flex flex-row justify-start mt-[5px] p-4 cursor-pointer">
+            <AiOutlineUser className="font-semibold text-xl block mt-[3px]" />
+            <p className="block text-lg font-semibold pl-[10px]">Users</p>
+          </div>
+          <div className="list-icon text-white flex flex-row justify-start mt-[5px] p-4 cursor-pointer">
+            <AiOutlineSetting className="font-semibold text-xl block mt-[3px]" />
+            <p className="block text-lg font-semibold pl-[10px]">Settings</p>
+          </div>
+          <div className="list-icon text-white flex flex-row justify-start mt-[5px] p-4 cursor-pointer">
+            <AiOutlineMessage className="font-semibold text-xl block mt-[3px]" />
+            <p className="block text-lg font-semibold pl-[10px]">Messages</p>
+          </div>
+          <div className="list-icon text-white flex flex-row justify-start mt-[5px] p-4 cursor-pointer">
+            <AiOutlineBell className="font-semibold text-xl block mt-[3px]" />
+            <p className="block text-lg font-semibold pl-[10px]">Notifications</p>
+          </div>
         </div>
-        <div className="list-icon text-white flex flex-row justify-center mt-[5px] p-4 cursor-pointer">
-          <AiOutlineUser className="font-semibold text-xl block mt-[3px]" />
-          <p className="block text-lg font-semibold pl-[10px]">Users</p>
-        </div>
-        <div className="list-icon text-white flex flex-row justify-center mt-[5px] p-4 cursor-pointer">
-          <AiOutlineSetting className="font-semibold text-xl block mt-[3px]" />
-          <p className="block text-lg font-semibold pl-[10px]">Settings</p>
-        </div>
-        <div className="list-icon text-white flex flex-row justify-center mt-[5px] p-4 cursor-pointer">
-          <AiOutlineMessage className="font-semibold text-xl block mt-[3px]" />
-          <p className="block text-lg font-semibold pl-[10px]">Messages</p>
-        </div>
-        <div className="list-icon text-white flex flex-row justify-center mt-[5px] p-4 cursor-pointer">
-          <AiOutlineBell className="font-semibold text-xl block mt-[3px]" />
-          <p className="block text-lg font-semibold pl-[10px]">Notifications</p>
-        </div>
+
+
       </div>
       <div className="content-area h-screen">
         <div className="heading">
-          <p className="text-emerald-500 text-2xl font-bold pl-[43px] pt-[20px]">Hi Micin</p>
+          <p className="text-emerald-500 text-2xl font-bold pl-[43px] pt-[20px] flex flex-row gap-2">
+            <div>Hi</div>
+            <MdOutlineWavingHand className="text-amber-300 mt-[4px]"/>
+            <div>Micin</div>
+             </p>
         </div>
         <div className="details mt-[50px]">
           <div className="detail-box border-r-2 border-l-2 border-b-2 border-neutral-400 w-[80%] mx-auto rounded-lg h-[80vh]">
-            <div className="progress-bar w-[80%] bg-slate-200 h-[15px] mt-[20px] mx-auto border-2 border-slate-200 rounded-lg"></div>
+            <div className="progress-bar w-[80%] bg-slate-200 h-[15px] mt-[20px] mx-auto rounded-lg">
+              <div className="w-[20%] bg-emerald-500 h-[100%] rounded-lg"></div>
+            </div>
             <div className="another-detailed-box w-[80%] h-[50%] mx-auto mt-[20px]">
               <p className="text-xl font-semibold">Revenue Forecasts?</p>
               <div className="year-list mt-4">
@@ -75,9 +88,9 @@ function App() {
               <div className="form-area bg-slate-100 border-2 border-slate-100 rounded-lg p-4 mt-4">
                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-row">
                   <div className="flex-1">
-                    <label htmlFor="year-select" className="block text-emerald-500 underline underline-offset-1 font-semibold">Choose Year</label>
+                    <label htmlFor="year-select" className="block text-emerald-500 underline underline-offset-1 font-semibold mb-[10px]">Choose Year</label>
 
-                    <select name="pets" id="year-select" {...register("year", { required: true })}>
+                    <select name="pets" id="year-select" {...register("year", { required: true })} className="py-[5px] px-[18px] rounded-lg bg-white">
                       <option value="1998">1998</option>
                       <option value="2008">2008</option>
                       <option value="2018">2018</option>
@@ -87,10 +100,10 @@ function App() {
                     </select>
                   </div>
                   <div className="flex-1">
-                    <p className="text-emerald-500 underline underline-offset-1 font-semibold">Revenue</p>
-                    <input type="number" {...register("revenue", { required: true })} placeholder="Enter Amount" />
+                    <p className="text-emerald-500 underline underline-offset-1 font-semibold mb-[10px]">Revenue</p>
+                    <input type="number" {...register("revenue", { required: true })} placeholder="Enter Amount" className="rounded-lg p-2"/>
                   </div>
-                  <div className="flex-1"><button type="submit" className="bg-emerald-500 text-white px-[20px] py-[3px] border-2 border-emerald-500 rounded-md block mx-auto mt-[8px]">ADD</button></div>
+                  <div className="flex-1"><button type="submit" className="bg-emerald-500 text-white px-[20px] py-[3px] border-2 border-emerald-500 rounded-md block mx-auto mt-[23px]">ADD</button></div>
                 </form>
               </div>
             </div>
